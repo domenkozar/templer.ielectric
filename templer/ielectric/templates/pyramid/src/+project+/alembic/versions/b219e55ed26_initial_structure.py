4eae2242def69add60a6ee00829236d03d2863db
Domen Kožar <domen@dev.si>
2012-09-06 12:25:32 +0200

    provide skeleton for first migration

---
+++ templer/ielectric/templates/pyramid/src/+project+/alembic/versions/b219e55ed26_initial_structure.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: b219e55ed26
+Revises: None
+Create Date: 2012-09-06 12:20:03.102521
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b219e55ed26'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
